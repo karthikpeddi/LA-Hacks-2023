fdd7b3d19e72b90816a564fc2d6b06b06c4a8c29
aherston <77765004+aherston@users.noreply.github.com>
2023-04-22 01:48:37 -0700

    import openai not working

---
+++ lahacks.py
@@ -0,0 +1,30 @@
+import openai
+import json
+
+# API key
+openai.api_key = "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"
+
+# parameters for API req
+model_engine = "davinci-x"
+prompt_text = "hi, how are you doing today?"
+temperature = 0.7
+max_tokens = 20
+
+# input language
+language = "en"
+
+# send API req
+response = openai.Completion.create(
+    engine = model_engine, 
+    prompt = prompt_text, 
+    temperature = temperature, 
+    max_tokens = max_tokens, 
+    language = language
+)
+
+# parse response
+response_text = response.choices[0].text.strip()
+response_text = response_text.replace("\n", " ")
+
+# print response
+print(response_text)
