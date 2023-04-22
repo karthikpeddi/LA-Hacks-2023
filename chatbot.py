472fea202a08f1f15cbda0b2bfe4b99f954c88bb
aherston <77765004+aherston@users.noreply.github.com>
2023-04-22 11:35:49 -0700

    preamble and chatbot w/o state management

--- chatbot.py
+++ chatbot.py
@@ -2,29 +2,32 @@ import openai
 import json
 
 # API key
-openai.api_key = "API key -- ask alice"
+openai.api_key
 
-# parameters for API req
-model_engine = "davinci-x"
-prompt_text = "hi, how are you doing today?"
+model_engine = "text-davinci-003"
 temperature = 0.7
 max_tokens = 20
 
-# input language
-language = "en"
+# inputs all strings
+def preamble(language, preamble_text, input_text):
+    prompt_text = "you are speaking" + language + "." + preamble_text
+    preamble_output = openai.Completion.create(
+        engine = model_engine, 
+        prompt = prompt_text, 
+        temperature = temperature, 
+        max_tokens = max_tokens
+    )
+    response_text = preamble_output.choices[0].text.strip()
+    return response_text.replace("\n", " ")
 
-# send API req
-response = openai.Completion.create(
-    engine = model_engine, 
-    prompt = prompt_text, 
-    temperature = temperature, 
-    max_tokens = max_tokens, 
-    language = language
-)
-
-# parse response
-response_text = response.choices[0].text.strip()
-response_text = response_text.replace("\n", " ")
-
-# print response
-print(response_text)
+def chatbot(text):
+    prompt_text = text
+    chatbot_response = openai.Completion.create(
+        engine = model_engine, 
+        prompt = prompt_text, 
+        temperature = temperature, 
+        max_tokens = max_tokens
+    )
+    # parse response
+    response_text = chatbot_response.choices[0].text.strip()
+    return response_text.replace("\n", " ")
